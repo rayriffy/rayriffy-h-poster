bb4a2e4d5cf018ddb06acedcab9be239974d8cb1
Phumrapee Limpianchop <git@rayriffy.com>
2019-12-10 16:16:50 +0700

    :bug: Manualy fetch URL

--- src/templates/poster/services/getCover.js
+++ src/templates/poster/services/getCover.js
@@ -3,5 +3,5 @@ import axios from 'axios'
 export const getCover = async id => {
   const res = await axios.get(`https://opener.now.sh/api/data/${id}`)
 
-  return res.data.images.cover.src
+  return `https://t.nhentai.net/galleries/${res.data.media_id}/cover.${res.data.images.cover.t === p ? 'png' : 'jpg'}`
 }
