63640c79f02979581d6b25523fba25aea5d72c91
Phumrapee Limpianchop <git@rayriffy.com>
2019-12-10 20:12:38 +0700

    :label: Type fix

--- src/pages/encode/[id].tsx
+++ src/pages/encode/[id].tsx
@@ -6,7 +6,7 @@ import Poster from '../../templates/poster/components'
 
 const EncodePage: React.FC = props => {
   const router = useRouter()
-  const { id } = router.query
+  const id = router.query.id as string
 
   return <React.Fragment>{id && <Poster id={id} />}</React.Fragment>
 }
