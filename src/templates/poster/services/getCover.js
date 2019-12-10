20ec2ba3b13ab65c8af5637c47d36afe97c05569
Phumrapee Limpianchop <git@rayriffy.com>
2019-12-10 16:20:12 +0700

    :recycle: Cleanup

--- src/templates/poster/services/getCover.js
+++ src/templates/poster/services/getCover.js
@@ -1,7 +1,7 @@
-import axios from 'axios'
+import fetch from 'isomorphic-unfetch'
 
 export const getCover = async id => {
-  const res = await axios.get(`https://opener.now.sh/api/data/${id}`)
+  const res = await fetch(`https://opener.now.sh/api/data/${id}`).then(o => o.json())
 
-  return `https://t.nhentai.net/galleries/${res.data.media_id}/cover.${res.data.images.cover.t === p ? 'png' : 'jpg'}`
+  return `https://t.nhentai.net/galleries/${res.media_id}/cover.${res.images.cover.t === 'p' ? 'png' : 'jpg'}`
 }
